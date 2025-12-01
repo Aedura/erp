87830f8a18c57e9f02d72174e50c96014a86ce5e
opdhaker <opdhaker2007@gmail.com>
2025-12-01 23:28:29 +0530

    cleared entire codebase because i fucked up

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-xl font-bold font-syne md:text-5xl hover:text-amber-200 hover:shadow-2xl transition duration-500">Coming Soon...</h1>
-    </div>
-  );
-}
