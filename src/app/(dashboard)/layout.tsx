4d3ae9e4cc921345452bf1ed61725844b542b381
opdhaker <opdhaker2007@gmail.com>
2025-12-02 17:37:51 +0530

    basic setup

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <div className="h-screen flex ">
+        {/* LEFT - sidebar*/}
+        <div className="w-1/6 bg-red-200">l</div>
+        {/* RIGHT */}
+        <div className="w-5/6 bg-blue-200">r</div>
+
+      </div>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ERP | Aedura",
+  description: "Next-Gen ERP System and architecture for K-12 equations",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
