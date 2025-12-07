d257176a646a1eddcb058bba3cba412194030fa2
opdhaker <opdhaker2007@gmail.com>
2025-12-08 03:10:04 +0530

    student page complete

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -15,7 +15,7 @@ const AdminPage = () => {
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
-          <UserCard type="staff" />
+          {/* <UserCard type="staff" /> */}
         </div>
         <div className="flex gap-4 flex-col lg:flex-row">
           <div className="w-full lg:w-1/3 h-[450px]">
@@ -31,12 +31,8 @@ const AdminPage = () => {
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <div className="">
-          <EvenetCalendar />
-        </div>
-        <div className="">
-          <Announcements />
-        </div>
+        <EvenetCalendar />
+        <Announcements />
       </div>
     </div>
   )
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,6 +1,21 @@
+import Announcements from "@/app/components/Announcements"
+import BigCalendar from "@/app/components/BigCalendar"
+import EvenetCalendar from "@/app/components/EvenetCalendar"
+
 const StudentPage = () => {
   return (
-    <div>StudentPage</div>
+    <div className="p-4 flex flex-col gap-4 xl:flex-row">
+      <div className="w-full xl:w-2/3 flex flex-col gap-8">
+        <div className="bg-background h-full p-4 rounded-lg shadow-md">
+          <h1 className="text-xl font-semibold">Schedule (10-B)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EvenetCalendar />
+        <Announcements />
+      </div>
+    </div>
   )
 }
 export default StudentPage
